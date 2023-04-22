71e90cc1431dfae399c078c3825589ba4eabfc00
Sean Bryant <smbryant1730@gmail.com>
2023-04-22 04:08:29 -0400

    finished all crud, it appears

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,8 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
 
 export default function ViewAuthor() {
+  const [authDetails, setAuthDetails] = useState({});
   const router = useRouter();
+
+  // TODO: grab firebaseKey from url
   const { firebaseKey } = router.query;
 
-  return <div>New Author Here {firebaseKey}</div>;
+  // TODO: make call to API layer to get the data
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthDetails);
+  }, [firebaseKey]);
+
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <img src={authDetails.image} alt={authDetails.title} style={{ width: '300px' }} />
+      </div>
+      <div className="text-white ms-5 details">
+        <h5>
+          {authDetails.first_name}
+          {authDetails.last_name}
+          {authDetails.authorObject?.favorite ? ' 🤍' : ''}
+        </h5>
+        Author Email: <a href={`mailto:${authDetails.email}`}>{authDetails.email}</a>
+        <hr />
+      </div>
+      <div className="d-flex flex-wrap">
+        {authDetails.books?.map((book) => (
+          <BookCard key={book.firebaseKey} bookObj={book} onUpdate={viewAuthorDetails} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- pages/authors.js
+++ pages/authors.js
@@ -1,3 +1,36 @@
-export default function CreateAuthor() {
-  return <h1>Create Author Page</h1>;
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getAuthors } from '../api/authorData';
+import { useAuth } from '../utils/context/authContext';
+import AuthorCard from '../components/AuthorCard';
+
+export default function AuthorsOnDom() {
+  const [authors, setAuthors] = useState([]);
+  const { user } = useAuth();
+
+  const getAllAuthors = () => {
+    getAuthors(user.uid).then(setAuthors);
+  };
+
+  useEffect(() => {
+    getAllAuthors();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <div className="text-center my-4">
+        <Link href="/author/new" passHref>
+          <Button>Add New Author</Button>
+        </Link>
+        <div className="d-flex flex-wrap">
+          {authors.map((author) => (
+            <AuthorCard key={author.firebaseKey} authorObj={author} onUpdate={getAllAuthors} />
+          ))}
+        </div>
+
+      </div>
+    </>
+  );
 }
